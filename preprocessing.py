5a077a628696aa87d9d0b95b969209f218f03c8c
OmarKouta73 <omarkouta551@gmail.com>
2024-05-10 22:22:21 +0300

    update data

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import numpy as np
 
 
 def encoding(dir):
     df = pd.read_csv(dir)
     df['label'] = df['label'].map({'Real': 0, 'AI': 1})
-    df.to_csv(dir)
+    df.to_csv(dir, index=False)
+
 
 encoding("data/Train/Train.csv")
 encoding("data/Valid/Validation.csv")
